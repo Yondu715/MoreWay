8d566b75424b874131c2244ecb2a461c1b1fa278
Yondu715 <maxv153218@gmail.com>
2024-04-10 14:47:49 +0700

    ref: add phpdoc

--- src/app/Infrastructure/WebSocket/Controllers/Friend/FriendNotifier.php
+++ src/app/Infrastructure/WebSocket/Controllers/Friend/FriendNotifier.php
@@ -7,7 +7,12 @@ use App\Infrastructure\WebSocket\Controllers\Friend\FriendWebSocketController;
 
 class FriendNotifier implements INotifier
 {
-    public function sendNotification(int $userId, mixed $notification)
+    /**
+     * @param int $userId
+     * @param mixed $notification
+     * @return void
+     */
+    public function sendNotification(int $userId, mixed $notification): void
     {
         FriendWebSocketController::sendNotification($userId, $notification);
     }
