f635e98c0aafed3579ba3f6fedb14c3a1105ce24
Yondu715 <maxv153218@gmail.com>
2024-04-16 02:51:10 +0700

    fix: comment on api execption constructor ???

--- src/app/Infrastructure/Exceptions/ApiException.php
+++ src/app/Infrastructure/Exceptions/ApiException.php
@@ -8,13 +8,13 @@ use Illuminate\Http\JsonResponse;
 class ApiException extends Exception
 {
 
-    public function __construct(string $message = '', mixed $code = 500)
-    {
-        parent::__construct($message, $code);
-        if (!is_int($code) || $code < 100 || $code > 599) {
-            $this->code = 500;
-        }
-    }
+    // public function __construct(string $message = '', mixed $code = 500)
+    // {
+    //     parent::__construct($message, $code);
+    //     if (!is_int($code) || $code < 100 || $code > 599) {
+    //         $this->code = 500;
+    //     }
+    // }
 
     public function render(): JsonResponse
     {
