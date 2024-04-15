4a2ed59fe0019be8fcb1f0e886e815f814bd4b8a
Yondu715 <maxv153218@gmail.com>
2024-04-15 12:54:01 +0700

    feat: add construct for api exception

--- src/app/Infrastructure/Exceptions/ApiException.php
+++ src/app/Infrastructure/Exceptions/ApiException.php
@@ -7,6 +7,17 @@ use Illuminate\Http\JsonResponse;
 
 class ApiException extends Exception
 {
+
+    public function __construct(string $message = '', int $code = 500)
+    {
+        $this->message = $message;
+        if ($code >= 400 && $code <= 599) {
+            $this->code = $code;
+        } else {
+            $this->code = 500;
+        }
+    }
+
     public function render(): JsonResponse
     {
         return response()->json([
