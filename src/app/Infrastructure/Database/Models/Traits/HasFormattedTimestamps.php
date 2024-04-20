808a4d9b82e05b7112e947253fd21840cf16cae4
Yondu715 <maxv153218@gmail.com>
2024-04-20 13:24:25 +0700

    feat: add trait for get formatted timestamps from model

---
+++ src/app/Infrastructure/Database/Models/Traits/HasFormattedTimestamps.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Src\App\Infrastructure\Database\Models\Traits;
+
+// Test trait
+trait HasFormattedTimestamps
+{
+    /**
+     * @return string
+     */
+    public function getCreatedAtAttribute(): string
+    {
+        return $this->created_at->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * @return string
+     */
+    public function getUpdatedAtAttribute(): string
+    {
+        return $this->updated_at->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeletedAtAttribute(): string
+    {
+        return $this->deleted_at->format('Y-m-d H:i:s');
+    }
+}
