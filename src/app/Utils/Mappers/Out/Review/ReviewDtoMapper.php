b36c31d792eac059ffa8fddbae544ee7572f3382
Yondu715 <maxv153218@gmail.com>
2024-04-20 13:27:44 +0700

    feat: remove Carbon; add formated time

--- src/app/Utils/Mappers/Out/Review/ReviewDtoMapper.php
+++ src/app/Utils/Mappers/Out/Review/ReviewDtoMapper.php
@@ -6,7 +6,6 @@ use App\Application\DTO\Out\Review\ReviewDto;
 use App\Infrastructure\Database\Models\PlaceReview;
 use App\Infrastructure\Database\Models\RouteReview;
 use App\Utils\Mappers\Out\Auth\UserDtoMapper;
-use Carbon\Carbon;
 
 class ReviewDtoMapper
 {
@@ -16,12 +15,11 @@ class ReviewDtoMapper
      */
     public static function fromReviewModel(PlaceReview|RouteReview $review): ReviewDto
     {
-        $createdAt = new Carbon($review->created_at);
         return new ReviewDto(
             id: $review->id,
             text: $review->text,
             rating: $review->rating,
-            createdAt: $createdAt->timestamp,
+            createdAt: $review->created_at->format('Y-m-d H:i:s'),
             author: UserDtoMapper::fromUserModel($review->author),
         );
     }
