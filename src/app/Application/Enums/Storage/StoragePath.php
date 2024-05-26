dfb66510e1b770ceb955d0ec113f701591bb5f47
Yondu715 <maxv153218@gmail.com>
2024-05-27 01:21:46 +0700

    ref: rename storage enum

---
+++ src/app/Application/Enums/Storage/StoragePath.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Application\Enums\Storage;
+
+enum StoragePath: string
+{
+    case UserAvatar = 'users';
+}
--- src/app/Application/Enums/Storage/StoragePaths.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Application\Enums\Storage;
-
-enum StoragePaths: string
-{
-    case UserAvatar = 'users/';
-}
