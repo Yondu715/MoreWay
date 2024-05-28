1ed63148338cdb4b41a3ba60502e3ea1f9a5d458
tleeen <peterivan5555@gmail.com>
2024-05-28 19:47:43 +0700

    feat: add exeption RouteNameIsTaken

--- src/app/Application/Exceptions/Route/RouteNameIsTaken.php
+++ src/app/Application/Exceptions/Route/RouteNameIsTaken.php
@@ -7,7 +7,7 @@ use App\Application\Exceptions\InternalException;
 class RouteNameIsTaken extends InternalException
 {
     /** @var int */
-    protected $code = 404;
+    protected $code = 409;
 
     /** @var string */
     protected $message = "Маршрут с таким названием уже существует";
